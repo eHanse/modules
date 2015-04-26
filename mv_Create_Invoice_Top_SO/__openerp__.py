da5a7db4129839e34ef2d7977ebda7b5893760a6
Yoshi Tashiro <tashiro@roomsfor.hk>
2015-04-26 21:29:47 +0800

    ABILBAÄHBÄN

---
+++ mv_Create_Invoice_Top_SO/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) Rooms For (Hong Kong) Limited T/A OSCG (<http://www.openerp-asia.net>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Adjustment on Sales Order Form View',
+    'version': '0.5',
+    'author': 'eHanse',
+    'website': 'https://www.ehanse.de',
+    'category': 'Not a Partner yet',
+    'depends': ["base",],
+    'description': """
+* Moves the Create Invoice Field from other Information on Sales Order to TOP 
+    """,
+    'data': [
+        'sale_view.xml',
+        'sale_stock_view.xml',
+    ],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
